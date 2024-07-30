6a0ab8ad9d46200f69a9c20aa3282608e74173e8
34RTHY <Sakchote.khe@gmail.com>
2024-07-31 01:20:12 +0700

    Update main.py
    allowed all origin

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ origins = [
 # Add CORSMiddleware to the app with restricted methods
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
