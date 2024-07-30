4e81bd20987917270ce74b65d589ba2e028140f9
34RTHY <Sakchote.khe@gmail.com>
2024-07-30 22:25:24 +0700

    Update main.py
    updated origin

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
-    "https://roadmap-planner-api.onrender.com",
+    "https://mapster-cogniticcore.onrender.com",
     "https://mapster-front-end.vercel.app",
 ]
 
